b6be7d4adae910ac4958e63d9d1df0e83fa2cc0d
Sasha Greene <sashagreene@hotmail.com>
2020-06-09 13:04:06 -0600

    initial

---
+++ Source/tank/tankGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "tankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANK_API AtankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
